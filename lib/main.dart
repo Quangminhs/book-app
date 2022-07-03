d6ef375ee57f738d7b19abadca08b15b2f0ea502
minhpq <quangminhs@gmail.com>
2022-07-03 22:19:00 +0700

    Add Sys chrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screen/home_screen.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() {
+  // SystemChrome.setSystemUIOverlayStyle(
+  //     SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
